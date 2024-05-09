a1d619edef5137c582e29dde59d533db18a935dd
Nexio-lbo <123646928+Nexio-lbo@users.noreply.github.com>
2024-05-09 18:52:05 +0200

    inscription

---
+++ inscription.php
@@ -0,0 +1,16 @@
+<inscription class="php">
+    <body>
+        <header>
+            <h1> Inscription </h1>
+        </header>
+        <u1>
+            <form action="inscription-from.php" method="POST"></form>
+            <input type="text" name="identifiant"></input>
+            <input type="text" name="mot de passe"></input>
+            <input type="text" name="e-mail"></input>
+        </u1>
+        <u2>
+            <input type="button" name="je m'inscris"></input>
+        </u2>
+    </body>
+</inscription>
