3751f0d34980a167dff1797c8841b3ef906bf4a8
vincent <lebouquinVR@hotmail.com>
2024-05-14 14:04:21 +0200

    feat(style): position and font

--- inscription_form.php
+++ inscription_form.php
@@ -13,11 +13,48 @@
     </header>
     <u1>
         <form action="inscription-from.php" method="POST"></form>
+        
+        <input type="submit" name="username"></input>
         <input type="text" name="username"></input>
-        <input type="text" name="password"></input>
-        <input type="text" name="mail"></input>
     </u1>
+
     <u2>
-        <input type="button" name="je m'inscris"></input>
+        <input type="submit" name="password"></input>
+        <input type="text" name="password"></input>
     </u2>
+    
+    <u3>
+        <input type="submit" name="mail"></input>
+        <input type="text" name="mail"></input>
+    </u3>
+    
+    <u4>
+        <input type="submit" name="je m'inscris"></input>
+        <input type="button" name="je m'inscris"></input>
+    </u4>
+
+    <style>
+        body
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: ghostwhite;
+        }
+        .inscription-container u1
+        {
+            text-align: left;
+        }
+        .inscription-container u2
+        {
+            text-align: center;
+        }
+        .inscription-container u3
+        {
+            text-align: right;
+        }
+        .inscription-container u4
+        {
+            text-align: center;
+            text-align: end;
+        }
+    </style>
 </body>
