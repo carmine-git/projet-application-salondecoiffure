cfe9071dde3086f491489284367907e230e7c5be
vincent <lebouquinVR@hotmail.com>
2024-05-15 16:52:37 +0200

    Chore(css):styles

--- inscription_form.php
+++ inscription_form.php
@@ -5,6 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: ghostwhite;
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+            margin-top: 100px;
+        }
+        header
+        {
+            text-align: center;
+            background-color: blue;
+            color: ghostwhite;
+        }
+    </style>
 </head>
 
 <body>
@@ -13,48 +33,27 @@
     </header>
     <u1>
         <form action="inscription-from.php" method="POST"></form>
-        
-        <input type="submit" name="username"></input>
+        <h3> Username : </h3>
         <input type="text" name="username"></input>
     </u1>
 
     <u2>
-        <input type="submit" name="password"></input>
+        <h3> Password : </h3>
         <input type="text" name="password"></input>
     </u2>
     
     <u3>
-        <input type="submit" name="mail"></input>
-        <input type="text" name="mail"></input>
+        <h3> E-mail : </h3>
+        <form type="text" name="mail"></form>
     </u3>
     
-    <u4>
-        <input type="submit" name="je m'inscris"></input>
+    <input>
+        <h3> Je m'inscris : </h3>
         <input type="button" name="je m'inscris"></input>
-    </u4>
-
-    <style>
-        body
-        {
-            font-family: Arial, Helvetica, sans-serif;
-            background-color: ghostwhite;
-        }
-        .inscription-container u1
-        {
-            text-align: left;
-        }
-        .inscription-container u2
-        {
-            text-align: center;
-        }
-        .inscription-container u3
-        {
-            text-align: right;
-        }
-        .inscription-container u4
-        {
-            text-align: center;
-            text-align: end;
-        }
-    </style>
+        <h6> J'accepte les conditions d'utilisation et la politique de confidentialité : </h6>
+        <label for="oui">oui</label>
+        <input type="checkbox" name="choice" id="oui"></input>
+        <label for="non">non</label>
+        <input type="checkbox" name="choise" id="non" ></input>
+    </input>
 </body>
