b8787e83b25f8e10b5e2cbf20d176dce6636cc61
vincent <lebouquinVR@hotmail.com>
2024-05-31 20:37:04 +0200

    commit-new25

---
+++ acceuil_form.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<lang="en"></lang>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body
+            {
+                font-family: Arial, Helvetica, sans-serif;
+                max-width: 500px;
+                margin: 0 auto;
+                padding: 0px;
+                background-color: ghostwhite;
+                border-radius: 5px;
+                box-shadow: 10px 10px 10px 10px rgba(0,0,0,0.1);
+                margin-top: 100px;
+                
+            }
+            
+            h1{
+                color: white;
+                background-color: black;
+                text-align: center;
+            }
+            u1
+            {
+                display: flex;
+                justify-content: center;
+            }
+            u2
+            {
+                display: flex;
+                justify-content: left;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <header>
+            <h1>Bienvenue au salon de coiffure</h1>
+        </header>
+        <u1>
+            <button type="button" name="inscription">Inscription</button>
+            <button type="button" name="identification">Identification</button>
+            <button type="button" name="calendrier">Calendrier</button>
+        </u1>
+        <u2>
+            
+        </u2>
+    </body>
+</html>
